f9e8c2bf9970aab70fdd833e50abae740a4cd8d4
Anshuman Kumar <mahianshuman491@gmail.com>
2023-12-06 22:10:54 +0530

    Start rating by using MUI

--- src/UI-Components/HomeComponent.js
+++ src/UI-Components/HomeComponent.js
@@ -4,6 +4,7 @@ import HeaderComponents from './HeaderComponents'
 import HomeSlider from './HomeSlider'
 import axios from 'axios'
 import HomeProductSlider from './HomeProductSlider'
+import Loader from './Loader'
 
 function HomeComponent() {
   const [productMobile, setProductMobile] = useState([]);
@@ -11,13 +12,16 @@ function HomeComponent() {
   const [productLaptop, setProductLaptop] = useState([]);
   const [productHeadphone, setProductHeadphone] = useState([]);
   const [productTelevision, setProductTelevision] = useState([]);
-  useEffect(()=>{
-    axios.get("http://localhost:5000/api/product").then((response)=>{
-      setProductMobile(response.data.filter((item)=>item.category === "mobile"))
-      setProductCamera(response.data.filter((item)=>item.category === "camera"))
-      setProductLaptop(response.data.filter((item)=>item.category === "laptop"))
-      setProductHeadphone(response.data.filter((item)=>item.category === "headphone"))
-      setProductTelevision(response.data.filter((item)=>item.category === "television"))
+  const [isLoading, setIsloading] = useState(false);
+  useEffect(() => {
+    setIsloading(true)
+    axios.get("http://localhost:5000/api/product").then((response) => {
+      setProductMobile(response.data.filter((item) => item.category === "mobile"))
+      setProductCamera(response.data.filter((item) => item.category === "camera"))
+      setProductLaptop(response.data.filter((item) => item.category === "laptop"))
+      setProductHeadphone(response.data.filter((item) => item.category === "headphone"))
+      setProductTelevision(response.data.filter((item) => item.category === "television"));
+      setIsloading(false)
     })
   }, []);
 
@@ -26,26 +30,35 @@ function HomeComponent() {
       <HeaderComponents />
       <WebNavBarComponent />
       <HomeSlider />
+      {
+        isLoading ? <Loader /> : <>
 
-      <section className='homeProduct__Containers'>
-        <h1 className='homeProduct__ContainersHeader'>best of mobiles</h1>
-       <HomeProductSlider productData={productMobile} category={"mobile"}/>
-      </section>
+         <section className='homeProduct__Containers'>
+            <h1 className='homeProduct__ContainersHeader'>best of mobile</h1>
+            <HomeProductSlider productData={productMobile} />
+          </section>
 
-      <section className='homeProduct__Containers'>
-        <h1 className='homeProduct__ContainersHeader'>best of laptop</h1>
-       <HomeProductSlider productData={productLaptop} category={"laptop"}/>
-      </section>
+          <section className='homeProduct__Containers'>
+            <h1 className='homeProduct__ContainersHeader'>best of laptop</h1>
+            <HomeProductSlider productData={productLaptop} />
+          </section>
 
-      <section className='homeProduct__Containers'>
-        <h1 className='homeProduct__ContainersHeader'>best of camera</h1>
-       <HomeProductSlider productData={productCamera} category={"camera"}/>
-      </section>
+          <section className='homeProduct__Containers'>
+            <h1 className='homeProduct__ContainersHeader'>best of camera</h1>
+            <HomeProductSlider productData={productCamera} />
+          </section>
 
-      <section className='homeProduct__Containers'>
-        <h1 className='homeProduct__ContainersHeader'>best of laptop</h1>
-       <HomeProductSlider productData={productHeadphone} category={"camera"}/>
-      </section>
+          <section className='homeProduct__Containers'>
+            <h1 className='homeProduct__ContainersHeader'>best of headphone</h1>
+            <HomeProductSlider productData={productHeadphone} />
+          </section>
+
+          <section className='homeProduct__Containers'>
+            <h1 className='homeProduct__ContainersHeader'>best of television</h1>
+            <HomeProductSlider productData={productTelevision} />
+          </section>
+        </>
+      }
 
     </>
   )
--- src/UI-Components/HomeProductSlider.js
+++ src/UI-Components/HomeProductSlider.js
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from 'react'
-
+import React, { useState } from 'react'
+import RatingComponent from './MUI-Components/RatingComponent';
 function HomeProductSlider({ productData, category }) {
 
-    let [currentIndex, setCurrentIndex] = useState(0);
+    let [currentIndex] = useState(0);
 
 
     return (
@@ -10,11 +10,15 @@ function HomeProductSlider({ productData, category }) {
             {/* <i className="fa-solid fa-caret-left leftArrow sliderArrow"></i>
             <i className="fa-solid fa-caret-right rightArrow sliderArrow"></i> */}
             <div className="homeProduct">
+                <i className="fa-regular fa-heart homeProductCommpnIConButton wishListButton"></i>
+                <i className="fa-regular fa-eye homeProductCommpnIConButton viewItemButton"></i>
                 <div className="homeProductPosterContainer">
                     <img src={productData[currentIndex]?.images[0]} alt="ProductPoster" className="ProductPoster" />
                 </div>
                 <div className="homeProduct__InformationContainer">
-                    
+                    <RatingComponent rating={productData[currentIndex]?.rating} />
+                    <span className='homeProduct__discountPercentageText'>{productData[currentIndex]?.discountPercentage} % Off</span>
+                    <button className='addToCartButton'>Add To Cart</button>
                 </div>
             </div>
 
@@ -22,11 +26,10 @@ function HomeProductSlider({ productData, category }) {
                 <div className="homeProductPosterContainer">
                     <img src={productData[++currentIndex]?.images[0]} alt="ProductPoster" className="ProductPoster" />
                 </div>
-            </div>
-
-            <div className="homeProduct">
-                <div className="homeProductPosterContainer">
-                    <img src={productData[++currentIndex]?.images[0]} alt="ProductPoster" className="ProductPoster" />
+                <div className="homeProduct__InformationContainer">
+                    <RatingComponent rating={productData[currentIndex]?.rating} />
+                    <span className='homeProduct__discountPercentageText'>{productData[currentIndex]?.discountPercentage} % Off</span>
+                    <button className='addToCartButton'>Add To Cart</button>
                 </div>
             </div>
 
@@ -34,11 +37,32 @@ function HomeProductSlider({ productData, category }) {
                 <div className="homeProductPosterContainer">
                     <img src={productData[++currentIndex]?.images[0]} alt="ProductPoster" className="ProductPoster" />
                 </div>
+                <div className="homeProduct__InformationContainer">
+                    <RatingComponent rating={productData[currentIndex]?.rating} />
+                    <span className='homeProduct__discountPercentageText'>{productData[currentIndex]?.discountPercentage} % Off</span>
+                    <button className='addToCartButton'>Add To Cart</button>
+                </div>
             </div>
 
             <div className="homeProduct">
                 <div className="homeProductPosterContainer">
                     <img src={productData[++currentIndex]?.images[0]} alt="ProductPoster" className="ProductPoster" />
+                </div>
+                <div className="homeProduct__InformationContainer">
+                    <RatingComponent rating={productData[currentIndex]?.rating} />
+                    <span className='homeProduct__discountPercentageText'>{productData[currentIndex]?.discountPercentage} % Off</span>
+                    <button className='addToCartButton'>Add To Cart</button>
+                </div>
+            </div>
+
+            <div className="homeProduct">
+                <div className="homeProductPosterContainer">
+                    <img src={productData[++currentIndex]?.images[0]} alt="ProductPoster" className="ProductPoster" />
+                </div>
+                <div className="homeProduct__InformationContainer">
+                    <RatingComponent rating={productData[currentIndex]?.rating} />
+                    <span className='homeProduct__discountPercentageText'>{productData[currentIndex]?.discountPercentage} % Off</span>
+                    <button className='addToCartButton'>Add To Cart</button>
                 </div>
             </div>
         </div>
